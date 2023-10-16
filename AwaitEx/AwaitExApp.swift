a177c047f701f10fc6c38ba8e988052a7a63d82d
Benji Loya <benjiloya.apple@gmail.com>
2023-10-17 00:21:51 +0500

    Initial Commit

---
+++ AwaitEx/AwaitExApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwaitExApp.swift
+//  AwaitEx
+//
+//  Created by Benji Loya on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AwaitExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
